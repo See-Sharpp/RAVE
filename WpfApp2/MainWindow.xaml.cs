0e652b92bdc05b001501df0a3dcfb4a5bffa006f
Scripter2104 <yashvsolanki81@gmail.com>
2025-05-24 18:06:18 +0530

    Initial commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            
+        }
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            string user = username.Text;
+            string pass = password.Password;
+
+            // Authenticate logic here
+            MessageBox.Show($"Logging in as {user}");
+
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+            this.Close();
+        }
+
+        private void ForgotPassword_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Redirecting to password recovery...");
+        }
+
+    }
+}
