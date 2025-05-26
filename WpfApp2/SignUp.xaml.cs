3fd689879202da866348e663ab8601294015b2cc
Scripter2104 <yashvsolanki81@gmail.com>
2025-05-26 19:58:12 +0530

    Validation of database

--- WpfApp2/SignUp.xaml.cs
+++ WpfApp2/SignUp.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using WpfApp2.Models;
 using WpfApp2.Context;
+using WpfApp2.Models;
 
 namespace WpfApp2
 {
@@ -37,29 +38,39 @@ namespace WpfApp2
 
         private async void Sign_Up(object sender, RoutedEventArgs e)
         {
-            
             SignUpDetail signUpDetail = new SignUpDetail
             {
                 Username = username.Text,
                 Email = email.Text,
                 Password = password.Password.ToString()
             };
- 
-            var ans = System.Windows.MessageBox.Show("Sign Up Successful!");
+
+            var validationResults = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            var context = new ValidationContext(signUpDetail);
+
+            bool isValid = Validator.TryValidateObject(signUpDetail, context, validationResults, true);
+
+            if (!isValid)
+            {
+             
+                string errorMessages = string.Join("\n", validationResults.Select(vr => "• " + vr.ErrorMessage));
+                System.Windows.MessageBox.Show(errorMessages, "Signup Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var ans = System.Windows.MessageBox.Show("Sign Up Successful!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
 
             if (ans == MessageBoxResult.OK)
             {
                 _context.SignUpDetails.Add(signUpDetail);
                 await _context.SaveChangesAsync();
+
                 MainWindow mainWindow = new MainWindow();
                 mainWindow.Show();
                 this.Close();
             }
-            else
-            {
-                System.Windows.MessageBox.Show("Sign Up Failed!");
-            }
         }
+
 
     }
 }
