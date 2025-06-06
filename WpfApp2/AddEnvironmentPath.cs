5738a21fe2aa32f70e6226f2627f95716018c2f9
Scripter2104 <yashvsolanki81@gmail.com>
2025-06-06 20:23:28 +0530

    added nircmd path to user variables

---
+++ WpfApp2/AddEnvironmentPath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class AddEnvironmentPath
+    {
+        public static void AddNircmd()
+        {
+            string? exeDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nircmd-x64");
+            string? systemPath = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.User);
+
+            if (!systemPath.Split(';').Any(p => p.Trim().Equals(exeDir, StringComparison.OrdinalIgnoreCase)))
+            {
+                string newPath = systemPath + ";" + exeDir;
+                Environment.SetEnvironmentVariable("PATH", newPath, EnvironmentVariableTarget.User);
+                Debug.WriteLine("NirCmd path added to **User PATH**.");
+            }
+            else
+            {
+                Debug.WriteLine("NirCmd path not added to **User PATH**.");
+            }
+        }
+
+    }
+}
+
