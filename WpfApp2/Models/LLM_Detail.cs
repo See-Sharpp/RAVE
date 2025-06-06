75838838262efacf0c83b1155776d01ac7da88c2
Vyom Shah <vyom509shah@gmail.com>
2025-06-06 18:49:28 +0530

    Database Changed

--- WpfApp2/Models/LLM_Detail.cs
+++ WpfApp2/Models/LLM_Detail.cs
@@ -15,19 +15,7 @@ namespace WpfApp2.Models
 
         [ForeignKey("UserId")]
         public SignUpDetail SignUpDetail { get; set; } = null!;
-        public string OriginalUserQuery { get; set; }
-        public string ProcessedUserQuery { get; set; }
-        public string PrimaryIntent { get; set; }
-        public string SpecificAction { get; set; }
-        public string FileDescription { get; set; }
-        public string FileTypeFilter { get; set; }
-        public string ApplicationName { get; set; }
-        public string SearchEngineQuery { get; set; }
-        public string SystemComponentTarget { get; set; }
-        public string SystemComponentValue { get; set; }
-        public string TaskDescriptionForSchedule { get; set; }
-        public string ScheduleDatetimeDescription { get; set; }
-        public string SystemCommand { get; set; }
-        public string TimeReferences { get; set; }
+         
+        public string? Expected_json { get; set; }=null!;
     }
 }
