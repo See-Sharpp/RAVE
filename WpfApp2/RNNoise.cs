01046c68fab5ee0bd380d5de9a2b4dbdfed57b0e
Vyom Shah <vyom509shah@gmail.com>
2025-06-22 19:31:39 +0530

    "Denoised the Audio"

---
+++ WpfApp2/RNNoise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class RNNoise
+    {
+        [DllImport("rnnoise.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr rnnoise_create();
+
+        [DllImport("rnnoise.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void rnnoise_destroy(IntPtr st);
+
+        [DllImport("rnnoise.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern float rnnoise_process_frame(IntPtr st, float[] outFrame, float[] inFrame);
+    }
+}
